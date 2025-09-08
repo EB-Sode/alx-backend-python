bb9ab57402b47a1b7e1ba124494c9f96ffcdf302
Blessed Sode <emi.square23@gmail.com>
2025-09-08 23:49:58 +0100

    alx demands

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,31 +1,30 @@
+import seed
+
 def stream_users_in_batches(batch_size):
     """
-    Generator function that yields user data in batches of a specified size.
-
-    Args:
-        batch_size (int): The number of user records to include in each batch.
-
-    Yields:
-        list: A list containing a batch of user records.
+    Generator that yields user data in batches directly from DB.
     """
-    stream_users = __import__('0-stream_users')
-    batch = []
-    for user in stream_users.stream_users():
-        batch.append(user)
-        if len(batch) == batch_size:
-            yield batch
-            batch = []
-    if batch:
-        yield batch
-
+    connection = seed.connect_to_prodev()
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM user_data;")
+        while True:
+            rows = cursor.fetchmany(batch_size)
+            if not rows:
+                break
+            yield rows
+        cursor.close()
+        connection.close()
 
 
 def batch_processing(batch_size):
     """
     Processes user data in batches and prints each batch.
-
-    Args:
-        batch_size (int): The number of user records to include in each batch.
     """
+
+    results = []
     for batch in stream_users_in_batches(batch_size):
-        print(batch)
+        for user in batch:
+            if user.age > 25:
+                results.append(user)
+    return results
