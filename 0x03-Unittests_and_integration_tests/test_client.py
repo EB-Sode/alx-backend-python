937b344003ae9367eabf989b5651b6d3b1a8bbe8
Blessed Sode <emi.square23@gmail.com>
2025-09-20 09:51:16 +0100

    reload task 2

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,6 +5,7 @@ from unittest import mock
 from parameterized import parameterized
 from client import GithubOrgClient
 
+
 class TestGithubOrgClient(unittest.TestCase):
     """Tests for GithubOrgClient"""
 
@@ -24,3 +25,21 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
+
+    def test_public_repos_url(self):
+        """Test that _public_repos_url returns repos_url from org payload"""
+        test_payload = {
+            "repos_url": "https://api.github.com/orgs/test-org/repos"
+            }
+
+        with mock.patch.object(
+            GithubOrgClient, "org",
+                new_callable=unittest.mock.PropertyMock
+        ) as mock_org:
+            mock_org.return_value = test_payload
+            client = GithubOrgClient("test-org")
+
+            result = client._public_repos_url
+            self.assertEqual(result, test_payload["repos_url"])
+
+            mock_org.assert_called_once()
